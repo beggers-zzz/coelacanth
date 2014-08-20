9d212625910cd774d7a97b992d15d8c0e46e1319
Ben Eggers <beggerss@cs.washington.edu>
2014-08-19 20:36:22 -0700

    started board tests

---
+++ test/core/Board_test.h
@@ -0,0 +1,17 @@
+/*
+
+Author: Ben Eggers <ben.eggers36@gmail.com>
+License: MIT
+
+Declaration of Board test suite.
+
+*/
+
+#ifndef _COELACANTH_BOARD_TEST_
+#define _COELACANTH_BOARD_TEST_
+
+#include <check.h>
+
+Suite *board_suite();
+
+#endif
