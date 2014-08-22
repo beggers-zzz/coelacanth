f38ad8fac8e87de36db8d54ac27ed125448ac86d
Ben Eggers <beggerss@cs.washington.edu>
2014-08-21 21:34:27 -0700

    pieces

---
+++ static/Pieces.c
@@ -0,0 +1,22 @@
+/*
+
+Author: Ben Eggers <ben.eggers36@gmail.com>
+License: MIT
+
+Pieces.
+
+*/
+
+
+extern const Piece WhitePawn = PAWN_MASK | WHITE_MASK;
+extern const Piece WhiteKnight = KNIGHT_MASK | WHITE_MASK;
+extern const Piece WhiteBishop = BISHOP_MASK | WHITE_MASK;
+extern const Piece WhiteRook = ROOK_MASK | WHITE_MASK;
+extern const Piece WhiteQueen = QUEEN_MASK | WHITE_MASK;
+extern const Piece WhiteKing = KING_MASK | WHITE_MASK;
+extern const Piece BlackPawn = PAWN_MASK | BLACK_MASK;
+extern const Piece BlackKnight = KNIGHT_MASK | BLACK_MASK;
+extern const Piece BlackBishop = BISHOP_MASK | BLACK_MASK;
+extern const Piece BlackRook = ROOK_MASK | BLACK_MASK;
+extern const Piece BlackQueen = QUEEN_MASK | BLACK_MASK;
+extern const Piece BlackKing = KING_MASK | BLACK_MASK;
---
+++ static/Pieces.h
@@ -0,0 +1,37 @@
+/*
+
+Author: Ben Eggers <ben.eggers36@gmail.com>
+License: MIT
+
+Pieces are like, important.
+
+*/
+
+#ifndef _COELACANTH_PIECES_
+#define _COELACANTH_PIECES_
+
+typedef uint8_t Piece;
+
+#define PAWN_MASK       0x01
+#define KNIGHT_MASK     0x02
+#define BISHOP_MASK     0x04
+#define ROOK_MASK       0x08
+#define QUEEN_MASK      0x10
+#define KING_MASK       0x20
+#define WHITE_MASK      0x40
+#define VIRGIN_MASK     0x80
+
+extern const Piece WhitePawn;
+extern const Piece WhiteKnight;
+extern const Piece WhiteBishop;
+extern const Piece WhiteRook;
+extern const Piece WhiteQueen;
+extern const Piece WhiteKing;
+extern const Piece BlackPawn;
+extern const Piece BlackKnight;
+extern const Piece BlackBishop;
+extern const Piece BlackRook;
+extern const Piece BlackQueen;
+extern const Piece BlackKing;
+
+#endif  // _COELACANTH_PIECES_
